4215a60ad260c860fdfb7f57545eec2b3f8f93f8
armory86 <camiseta80@gmail.com>
2021-06-12 11:08:41 -0500

    Compile with Proton Clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,9 +44,11 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_surya
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/surya_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
-TARGET_KERNEL_USE_LATEST_CLANG := true
-#TARGET_KERNEL_CLANG_VERSION := r416183b
+TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
